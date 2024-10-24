c2aec84c29946dbf745793d4362fa95f2b4d1ae1
NeoMashego <mashegonoj@gmail.com>
2024-10-24 13:11:26 +0200

    if statement to check for empty entries

--- scripts.js
+++ scripts.js
@@ -6,4 +6,8 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor(dividend / divider);
+  if (entries === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return false;   //not supposed to perform calculation
+  }
 });
